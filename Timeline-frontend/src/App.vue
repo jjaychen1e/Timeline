d31dbcd0e26da5a40f3c0f4144868e8dfa143547
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-10-31 23:13:07 +0800

    Initial commit

---
+++ Timeline-frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header'
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
