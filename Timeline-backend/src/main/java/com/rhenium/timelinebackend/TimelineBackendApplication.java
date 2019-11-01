b8e6c219896d0fba6bc977e88e4557ca06423154
HavenTong <852092786@qq.com>
2019-11-01 15:26:20 +0800

    backend configuration done

---
+++ Timeline-backend/src/main/java/com/rhenium/timelinebackend/TimelineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rhenium.timelinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimelineBackendApplication.class, args);
+    }
+
+}
---
+++ Timeline-backend/src/test/java/com/rhenium/timelinebackend/TimelineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhenium.timelinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
