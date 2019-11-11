415ab44fa2aa31f1be1a7fc0b79e879426275446
JJAYCHEN <10175101148@stu.ecnu.edu.cn>
2019-11-11 15:21:01 +0800

    Update Header.vue

--- Timeline-frontend/src/components/Header.vue
+++ Timeline-frontend/src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div style="float: right">
+    <div class="row" style="float: right">
       <el-button
         type="primary"
         @click="$emit('setDialogVisible', true)"
         icon="el-icon-chat-dot-round"
       >NEW POST</el-button>
     </div>
-    <div class="row text-center text-white mb-5" style="margin-top: 50px;">
+    <div class="row text-center text-white mb-5" style="clear: both" >
       <div class="col-lg-8 mx-auto">
         <h1 class="display-4">Timeline</h1>
         <p class="lead mb-0">This is a simple timeline demo for software testing.</p>
