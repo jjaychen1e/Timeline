aebbb643e06e2e727e5907895997b8f4281b5c4d
HavenTong <852092786@qq.com>
2019-11-27 20:25:11 +0800

    add TimelineService Interface

---
+++ Timeline-backend/src/main/java/com/rhenium/timelinebackend/service/TimelineItemServiceImpl.java
@@ -0,0 +1,81 @@
+package com.rhenium.timelinebackend.service;
+
+import com.rhenium.timelinebackend.bean.TimelineItem;
+import com.rhenium.timelinebackend.mapper.TimelineItemMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author HavenTong
+ * @date 2019/11/1 3:47 下午
+ */
+@Service
+@Transactional
+public class TimelineItemServiceImpl implements TimelineItemService {
+
+    @Resource
+    TimelineItemMapper timelineItemMapper;
+
+    @Override
+    public List<TimelineItem> getInitialTimelineItem(int count) {
+        return timelineItemMapper.getInitialTimelineItem(count);
+    }
+
+    /**
+     * @param type  0 means update, 1 means get earlier messages.
+     * @param id    when type == 0, id is the id of latest message
+     *              from user's site. when type == 1, id is the id
+     *              of the earliest message from user's site.
+     * @param count how many messages returned(if there are enough
+     *              messages from the database).
+     * @return
+     */
+    @Override
+    public List<TimelineItem> getMoreTimelineItem(int type, int id, int count) {
+        if (type == 0) {
+            return timelineItemMapper.getNewTimelineItem(id);
+        } else {
+            List<TimelineItem> oldTimelineItems = timelineItemMapper.getOldTimelineItem(id, count);
+            return oldTimelineItems;
+        }
+    }
+
+    @Override
+    public int addTimelineItem(String userName, String title, String text) {
+        String imageUrl = null;
+        TimelineItem timelineItem = new TimelineItem(0, userName, title, LocalDateTime.now(), text, imageUrl);
+        return timelineItemMapper.addTimelineItem(timelineItem);
+    }
+
+    @Override
+    public int addTimelineItem(String userName, String title, String text, MultipartFile file) {
+        String pathName = "/data/images/";
+        String fName = file.getOriginalFilename();
+        pathName += fName;
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(pathName);
+            fos.write(file.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String imageUrl = "http://152.136.173.30/images/" + fName;
+        TimelineItem timelineItem = new TimelineItem(0, userName, title, LocalDateTime.now(), text, imageUrl);
+        return timelineItemMapper.addTimelineItem(timelineItem);
+    }
+}
--- Timeline-backend/src/test/java/com/rhenium/timelinebackend/controller/TimelineItemControllerTest.java
+++ Timeline-backend/src/test/java/com/rhenium/timelinebackend/controller/TimelineItemControllerTest.java
@@ -1,27 +1,15 @@
 package com.rhenium.timelinebackend.controller;
 
-import com.rhenium.timelinebackend.mapper.TimelineItemMapper;
 import com.rhenium.timelinebackend.service.TimelineItemService;
-import org.junit.jupiter.api.DisplayName;
+import com.rhenium.timelinebackend.service.TimelineItemServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-
-import javax.swing.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
@@ -42,14 +30,14 @@ class TimelineItemControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    TimelineItemService timelineItemService;
+    TimelineItemServiceImpl timelineItemServiceImpl;
 
     @Test
     void shouldGetInitialItems() throws Exception{
         ResultActions perform = mockMvc.perform(get("/InitialTimelineItems")
                 .param("count", "4"));
         perform.andExpect(status().isOk());
-        verify(timelineItemService, times(1)).getInitialTimelineItem(4);
+        verify(timelineItemServiceImpl, times(1)).getInitialTimelineItem(4);
     }
 
     @Test
@@ -59,7 +47,7 @@ class TimelineItemControllerTest {
                                                 .param("id", "5")
                                                 .param("count", "5"));
         perform.andExpect(status().isOk());
-        verify(timelineItemService, times(1)).getMoreTimelineItem(1, 5, 5);
+        verify(timelineItemServiceImpl, times(1)).getMoreTimelineItem(1, 5, 5);
     }
 
     @Test
@@ -69,7 +57,7 @@ class TimelineItemControllerTest {
                 .param("id", "7")
                 .param("count", "7"));
         perform.andExpect(status().isOk());
-        verify(timelineItemService, times(1)).getMoreTimelineItem(0, 7, 7);
+        verify(timelineItemServiceImpl, times(1)).getMoreTimelineItem(0, 7, 7);
     }
 
     @Test
@@ -80,7 +68,7 @@ class TimelineItemControllerTest {
                 .param("text", "test case")
                 .param("file", ""));
         perform.andExpect(status().isOk());
-        verify(timelineItemService, times(1)).addTimelineItem("user", "test", "test case");
+        verify(timelineItemServiceImpl, times(1)).addTimelineItem("user", "test", "test case");
     }
 
     @Test
@@ -92,7 +80,7 @@ class TimelineItemControllerTest {
                 .param("title", "text")
                 .param("text", "text case"));
         perform.andExpect(status().isOk());
-        verify(timelineItemService, times(1)).addTimelineItem("user", "text","text case", mockFile);
+        verify(timelineItemServiceImpl, times(1)).addTimelineItem("user", "text","text case", mockFile);
     }
 
 }
